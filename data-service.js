94e1109adee3cf91287b2762cd808b226e88d0f7
Vimal Sunkria <varun66502@gmail.com>
2023-03-24 23:11:38 -0400

    assignment 5

--- data-service.js
+++ data-service.js
@@ -1,69 +1,283 @@
+const Sequelize = require("sequelize");
 
+var sequelize = new Sequelize(
+  "diaxvjzb",
+  "diaxvjzb",
+  "zgXLKHu7NWwyi3O2URTMDeidvyOQ1uD8",
+  {
+    host: "raja.db.elephantsql.com",
+    dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+    query: { raw: true },
+  }
+);
 
-var students = [];
-var programs =[];
-const fs = require('fs');
+var Student = sequelize.define("Student", {
+  studentID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: Sequelize.STRING,
+  phone: Sequelize.STRING,
+  addressStreet: Sequelize.STRING,
+  addressCity: Sequelize.STRING,
+  addressState: Sequelize.STRING,
+  addressPostal: Sequelize.STRING,
+  isInternationalStudent: Sequelize.BOOLEAN,
+  expectedCredential: Sequelize.STRING,
+  status: Sequelize.STRING,
+  registrationDate: Sequelize.STRING,
+});
 
-module.exports.initialize =function (){
-     
-    return new Promise(function(resolve, reject){
-        try{
-            fs.readFile('./data/students.json', (err, data)=>{
-                if(err) throw err;
-                students = JSON.parse(data);
-            });
-            fs.readFile('./data/programs.json', (err, data)=>{
-                if(err) throw err;
-                programs = JSON.parse(data);
-            });
-        }catch(ex){
-            reject('Unable to read file!');
-        }
-        resolve("")
-    });
+var Image = sequelize.define("Image", {
+  imageId: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  imageUrl: Sequelize.STRING,
+  version: Sequelize.INTEGER,
+  width: Sequelize.INTEGER,
+  height: Sequelize.INTEGER,
+  format: Sequelize.STRING,
+  resourceType: Sequelize.STRING,
+  uploadedAt: Sequelize.DATE,
+  originalFileName: Sequelize.STRING,
+  mimeType: Sequelize.STRING,
+});
 
-}
+var Program = sequelize.define("Program", {
+  programCode: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  programName: Sequelize.STRING,
+});
 
+Program.hasMany(Student, { foreignKey: "program" });
 
-
-
-module.exports.getAllStudents = function(){
-    var Allstudents =[];
-    return new Promise((resolve, reject)=>{
-        for(var i=0; i<students.length; i++){
-            Allstudents.push(students[i]);
-        }
-        if(Allstudents.length ==0)
-            reject("No result returned");
-        resolve(Allstudents);
-    });
-
+module.exports.initialize = function () {
+  return new Promise(function (resolve, reject) {
+    sequelize
+      .sync()
+      .then(function () {
+        resolve();
+      })
+      .catch(function (err) {
+        console.log(err);
+        reject("unable to sync the database");
+      });
+  });
 };
 
-module.exports.getInternationalStudents= function(){
-    var InternationalStudentsArray =[];
-    return new Promise(function(resolve, reject){
-        for(var i=0; i<students.length; i++){
-           if(students[i].isInternationalStudent ==true){
-            InternationalStudentsArray.push(students[i]);
-           }
-                
-        }
-        if(InternationalStudentsArray.length ==0)
-            reject("No result returned");
-        resolve(InternationalStudentsArray);
-    });
+module.exports.getAllStudents = function () {
+  return new Promise(function (resolve, reject) {
+    Student.findAll({ order: ["studentID"] })
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
 };
 
-
-module.exports.getPrograms= function(){
-   
-    return new Promise(function(resolve, reject){
-       if(programs.length ==0){
-           reject('No results returned!')
-       }else{
-           resolve(programs);
-       }
-    });
+module.exports.getStudentsByStatus = function (status) {
+  return new Promise(function (resolve, reject) {
+    Student.findAll({ where: { status: status } })
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
 };
 
+module.exports.getStudentsByProgramCode = function (program) {
+  return new Promise(function (resolve, reject) {
+    Student.findAll({ where: { program: program } })
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+
+module.exports.getStudentsByExpectedCredential = function (credential) {
+  return new Promise(function (resolve, reject) {
+    Student.findAll({ where: { expectedCredential: credential } })
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+
+module.exports.getStudentById = function (sid) {
+  return new Promise(function (resolve, reject) {
+    Student.findAll({ where: { studentID: sid } })
+      .then(function (data) {
+        resolve(data[0]);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+
+module.exports.getPrograms = function () {
+  return new Promise(function (resolve, reject) {
+    Program.findAll({ order: ["programCode"] })
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+
+module.exports.addStudent = function (studentData) {
+  return new Promise(function (resolve, reject) {
+    studentData.isInternationalStudent = studentData.isInternationalStudent
+      ? true
+      : false;
+
+    Object.keys(studentData).map(function (key) {
+      studentData[key] = studentData[key] == "" ? null : studentData[key];
+    });
+
+    Student.create(studentData)
+      .then(function () {
+        resolve("Student created successfully");
+      })
+      .catch(function () {
+        reject("unable to create student");
+      });
+  });
+};
+
+module.exports.updateStudent = function (studentData) {
+  return new Promise(function (resolve, reject) {
+    studentData.isInternationalStudent = studentData.isInternationalStudent
+      ? true
+      : false;
+
+    Object.keys(studentData).map(function (key) {
+      studentData[key] = studentData[key] == "" ? null : studentData[key];
+    });
+
+    Student.update(studentData, { where: { studentID: studentData.studentID } })
+      .then(function () {
+        resolve("Student updated successfully");
+      })
+      .catch(function () {
+        reject("Unable to update student");
+      });
+  });
+};
+
+module.exports.addImage = function (imageData) {
+  return new Promise(function (resolve, reject) {
+    Image.create(imageData)
+      .then(function () {
+        resolve("Image created successfully");
+      })
+      .catch(function (err) {
+        reject("Unable to create image");
+      });
+  });
+};
+
+module.exports.getImages = function () {
+  return new Promise(function (resolve, reject) {
+    Image.findAll()
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+
+module.exports.addProgram = function (programData) {
+  return new Promise(function (resolve, reject) {
+    Object.keys(programData).map(function (key) {
+      programData[key] = programData[key] == "" ? null : programData[key];
+    });
+
+    Program.create(programData)
+      .then(function () {
+        resolve("Program created successfully");
+      })
+      .catch(function (err) {
+        reject("unable to create program");
+        console.log(err);
+      });
+  });
+};
+
+module.exports.updateProgram = function (programData) {
+  return new Promise(function (resolve, reject) {
+    Object.keys(programData).map(function (key) {
+      programData[key] = programData[key] == "" ? null : programData[key];
+    });
+
+    Program.update(programData, {
+      where: { programCode: programData.programCode },
+    })
+      .then(() => {
+        resolve("Program updated successfully");
+      })
+      .catch(function (err) {
+        reject("Unable to update student: " + err);
+      });
+  });
+};
+
+module.exports.getProgramByCode = function (pcode) {
+  return new Promise(function (resolve, reject) {
+    Program.findAll({ where: { programCode: pcode } })
+      .then(function (data) {
+        resolve(data[0]);
+      })
+      .catch(function () {
+        reject("no results returned");
+      });
+  });
+};
+
+module.exports.deleteProgramByCode = function (pcode) {
+  return new Promise(function (resolve, reject) {
+    Program.destroy({ where: { programCode: pcode } })
+      .then(function () {
+        resolve("Program deleted successfully");
+      })
+      .catch(function () {
+        reject("Unable to delete program");
+      });
+  });
+};
+
+module.exports.deleteStudentById = function (id) {
+  return new Promise(function (resolve, reject) {
+    Student.destroy({ where: { studentID: id } })
+      .then(function () {
+        resolve("Student deleted successfully");
+      })
+      .catch(function (err) {
+        reject("Unable to delete student");
+      });
+  });
+};
--- server.js
+++ server.js
@@ -1,82 +1,347 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name: Vimal Sunkria Student ID:  158576215 Date: 03-02-2023
-*
-*  Online (Cyclic) Link: 
-*
-********************************************************************************/ 
+ *  WEB322 – Assignment 05
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+ *  assignment has been copied manually or electronically from any other source (including web sites) or
+ *  distributed to other students.
+ *
+ *  Name: Vimal Sunkria  Student ID: ______________ Date: 24 March, 2023
+ *
+ *  Online (Cyclic) Link:
+ *
+ ********************************************************************************/
 
 var express = require("express");
+const multer = require("multer");
+const streamifier = require("streamifier");
+const exphbs = require("express-handlebars");
+const cloudinary = require("cloudinary").v2;
+
 var app = express();
-var path= require("path");
-var dataService = require("./data-service.js");
+var HTTP_PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-function onHttpStart() {
+const dataService = require("./data-service");
 
-    console.log("Express http server listening on "+ PORT);
-    return new Promise(function(reslove, reject){
-        dataService.initialize().then(function(value){
-            console.log(value);
-        }).catch(function(reason){
-                console.log(reason);
-            });
+cloudinary.config({
+  cloud_name: "dlei8hlyj",
+  api_key: "225788388644212",
+  api_secret: "iiZ3toH-zUQmecUMbSKWqQMw4ZU",
+});
+const upload = multer();
+
+app.engine(".hbs", exphbs.engine({ extname: "hbs" }));
+
+app.set("view engine", ".hbs");
+
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+
+    helpers: {
+      navLink: function (url, options) {
+        return (
+          "<li" +
+          (url == app.locals.activeRoute ? ' class="active" ' : "") +
+          '><a href="' +
+          url +
+          '">' +
+          options.fn(this) +
+          "</a></li>"
+        );
+      },
+      equal: function (lvalue, rvalue, options) {
+        if (arguments.length < 3)
+          throw new Error("Handlebars Helper equal needs 2 parameters");
+        if (lvalue != rvalue) {
+          return options.inverse(this);
+        } else {
+          return options.fn(this);
+        }
+      },
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/students/add", (req, res) => {
+  dataService
+    .getPrograms()
+    .then((data) => res.render("addStudent", { programs: data }))
+    .catch((err) => res.render("addStudent", { programs: [] }));
+});
+
+app.get("/students", (req, res) => {
+  const status = req.query.status;
+  const program = req.query.program;
+  const credential = req.query.credential;
+
+  if (status) {
+    dataService
+      .getStudentsByStatus(status)
+      .then((students) => {
+        if (students.length > 0) res.render("students", { students });
+        else res.render("students", { message: "No results" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send("Error retrieving students");
+      });
+  } else if (program) {
+    dataService
+      .getStudentsByProgramCode(program)
+      .then((students) => {
+        if (students.length > 0) res.render("students", { students });
+        else res.render("students", { message: "No results" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send("Error retrieving students");
+      });
+  } else if (credential) {
+    dataService
+      .getStudentsByExpectedCredential(credential)
+      .then((students) => {
+        if (students.length > 0) res.render("students", { students });
+        else res.render("students", { message: "No results" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send("Error retrieving students");
+      });
+  } else {
+    dataService
+      .getAllStudents()
+      .then((students) => {
+        if (students.length > 0) res.render("students", { students });
+        else res.render("students", { message: "No results" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).send("Error retrieving students");
+      });
+  }
+});
+
+app.get("/students/delete/:studentID", (req, res) => {
+  const studentID = req.params.studentID;
+  dataService
+    .deleteStudentById(studentID)
+    .then(() => res.redirect("/students"))
+    .catch(() =>
+      res.status(500).send("Unable to Remove Student / Student not found")
+    );
+});
+
+app.post("/student/update", (req, res) => {
+  console.log(req.body.studentID);
+  dataService
+    .updateStudent(req.body)
+    .then(() => {
+      res.redirect("/students");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
     });
-
-}
-  
-
-
-app.use(express.static('public'));
-
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.get("/about", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/about.html"));
-});
-
-
-app.get("/students", function(req, res){
-    dataService.getAllStudents().then((data)=>{
-        res.json(data);
-    }).catch((error)=>{
-        res.json({message, error})
+app.post("/students/add", function (req, res) {
+  dataService
+    .addStudent(req.body)
+    .then(() => {
+      console.log("Student added");
+      res.redirect("/students");
+    })
+    .catch((err) => {
+      res.status(500).send("Unable to add student");
     });
-    
 });
 
-app.get("/InternationalStudents", function(req,res){
-   
-   dataService.getInternationalStudents().then(function(data){
-        res.json(data);
-   }).catch(function(err){
-       res.json({message: err});
-   });
+app.get("/student/:studentId", (req, res) => {
+  let viewData = {};
 
- 
- });
+  dataService
+    .getStudentById(req.params.studentId)
+    .then((data) => {
+      if (data) {
+        viewData.student = data; //store student data in the "viewData" object as "student"
+      } else {
+        viewData.student = null; // set student to null if none were returned
+      }
+    })
+    .catch(() => {
+      viewData.student = null; // set student to null if there was an error
+    })
+    .then(dataService.getPrograms)
+    .then((data) => {
+      viewData.programs = data; // store program data in the "viewData" object as "programs"
 
+      // loop through viewData.programs and once we have found the programCode that matches
+      // the student's "program" value, add a "selected" property to the matching
+      // viewData.programs object
 
-app.get("/programs", function(req,res){
-    
-    dataService.getPrograms().then(function(data){
-        res.json(data);
-    }).catch(function(err){
-        res.json({message: err});
+      for (let i = 0; i < viewData.programs.length; i++) {
+        if (viewData.programs[i].programCode == viewData.student.program) {
+          viewData.programs[i].selected = true;
+        }
+      }
+    })
+    .catch(() => {
+      viewData.programs = []; // set programs to empty if there was an error
+    })
+    .then(() => {
+      if (viewData.student == null) {
+        // if no student - return an error
+        res.status(404).send("Student Not Found");
+      } else {
+        res.render("student", { viewData: viewData }); // render the "student" view
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Unable to Show Students");
     });
-   
 });
 
-app.use(function(req, res){
-    res.status(404).send("PAGE NOT FOUND!!!!!!!!!!!");
+app.get("/programs/add", (req, res) => {
+  res.render("addProgram");
 });
 
-app.listen(PORT, onHttpStart);
+app.get("/programs", (req, res) => {
+  dataService
+    .getPrograms()
+    .then((programs) => {
+      if (programs.length > 0) res.render("programs", { programs });
+      else res.render("programs", { message: "No results found." });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.render("programs", { message: "An error occurred." });
+    });
+});
 
+app.get("/program/:programCode", (req, res) => {
+  dataService
+    .getProgramByCode(req.params.programCode)
+    .then((data) => {
+      if (data) res.render("program", { program: data });
+      else res.status(404).send("Program Not Found");
+    })
+    .catch(() => res.status(404).send("Program Not Found"));
+});
 
+app.get("/programs/delete/:programCode", (req, res) => {
+  dataService
+    .deleteProgramByCode(req.params.programCode)
+    .then(() => res.redirect("/programs"))
+    .catch((err) =>
+      res.status(500).send("Unable to Remove Program / Program not found")
+    );
+});
+
+app.post("/program/update", (req, res) => {
+  dataService
+    .updateProgram(req.body)
+    .then(() => res.redirect("/programs"))
+    .catch((error) => res.json({ message: err }));
+});
+
+app.post("/programs/add", function (req, res) {
+  dataService
+    .addProgram(req.body)
+    .then(() => res.redirect("/programs"))
+    .catch((err) => res.json({ message: err }));
+});
+
+app.get("/images/add", (req, res) => {
+  res.render("addImage");
+});
+
+app.get("/images", function (req, res) {
+  dataService
+    .getImages()
+    .then(function (images) {
+      if (images.length > 0) res.render("images", { images });
+      else res.render("images", { message: "no results" });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render("images", { message: "An error occurred." });
+    });
+});
+
+app.post("/images/add", upload.single("imageFile"), function (req, res) {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req).then((uploaded) => {
+      processForm(uploaded);
+    });
+  } else {
+    processForm("");
+  }
+
+  function processForm(uploaded) {
+    let imgData = {};
+    imgData.imageId = uploaded.public_id;
+    imgData.imageUrl = uploaded.url;
+    imgData.version = uploaded.version;
+    imgData.width = uploaded.width;
+    imgData.height = uploaded.height;
+    imgData.format = uploaded.format;
+    imgData.resourceType = uploaded.resource_type;
+    imgData.uploadedAt = uploaded.created_at;
+    imgData.originalFileName = req.file.originalname;
+    imgData.mimeType = req.file.mimetype;
+
+    dataService
+      .addImage(imgData)
+      .then(() => res.redirect("/images"))
+      .catch(() => res.status(500).send("Error adding image"));
+  }
+});
+
+app.use(function (req, res, next) {
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = route == "/" ? "/" : route.replace(/\/$/, "");
+  next();
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>Error: 404 Page Not Found</h1>");
+});
+
+dataService
+  .initialize()
+  .then(() =>
+    app.listen(HTTP_PORT, () =>
+      console.log("server listening on: http://localhost:" + HTTP_PORT + "/")
+    )
+  )
+  .catch((err) => console.log(err));
